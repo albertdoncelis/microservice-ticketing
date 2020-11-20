11f883cc3ba23729c28b7ab3153baea5c319bfea
Albert Don Celis <celisalbertdon@gmail.com>
2020-11-20 15:20:05 +0800

    create listener class

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
 import nats, {Message, Stan} from 'node-nats-streaming'
 import {randomBytes} from "crypto";
+import {TicketCreatedListener} from "./events/ticket-created-listener";
 
 console.clear()
 const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
@@ -18,56 +19,3 @@ stan.on('connect', () => {
 
 process.on('SIGINT', () => stan.close())
 process.on('SIGTERM', () => stan.close())
-
-abstract class Listener {
-  abstract subject: string
-  abstract queueGroupName: string
-  protected actWait = 5 * 1000
-
-  constructor(private readonly client: Stan) {}
-
-  abstract onMessage(data: any, msg: Message): void
-
-  subscriptionOptions() {
-    // manual acknowledgement of the events
-    // the default is false as soon the event received we will not received the event again.
-    return this.client.subscriptionOptions()
-      .setDeliverAllAvailable()
-      .setManualAckMode(true)
-      .setAckWait(this.actWait)
-      .setDurableName(this.queueGroupName)
-  }
-
-  listen() {
-    const subscription = this.client.subscribe(
-      this.subject,
-      this.queueGroupName,
-      this.subscriptionOptions()
-    )
-
-    subscription.on('message', (msg: Message) => {
-      console.log(
-        `Message Received: ${this.subject} / ${this.queueGroupName}`
-      )
-
-      const parsedData = this.parseMessage(msg)
-
-      this.onMessage(parsedData, msg)
-    })
-  }
-
-  parseMessage(msg: Message) {
-    const data = msg.getData()
-    return typeof data === 'string'? JSON.parse(data): JSON.parse(data.toString('utf8'))
-  }
-}
-
-class TicketCreatedListener extends Listener {
-  queueGroupName: string = 'payments-service';
-  subject: string = 'ticket:created';
-
-  onMessage(data: any, msg: Message): void {
-    console.log('Event data!', data)
-    msg.ack()
-  }
-}
