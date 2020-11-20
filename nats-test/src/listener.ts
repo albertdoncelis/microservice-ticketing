1ea9d54bf889c1ef618090c1f204f15c7120ad36
Albert Don Celis <celisalbertdon@gmail.com>
2020-11-20 12:36:51 +0800

    listener abstract class

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,4 +1,4 @@
-import nats, {Message} from 'node-nats-streaming'
+import nats, {Message, Stan} from 'node-nats-streaming'
 import {randomBytes} from "crypto";
 
 console.clear()
@@ -12,25 +12,62 @@ stan.on('connect', () => {
     console.log('NATS connection closed!')
     process.exit();
   })
-  // manual acknowledgement of the events
-  // the default is false as soon the event received we will not received the event again.
-  const options = stan.subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable() // restore all the events
-    .setDurableName('account-service') // create a container named account-service and saved all the events there (mni database)
 
-  const subscription = stan.subscribe('ticket:created','queue-group-name', options)
-
-  subscription.on('message', (msg: Message) => {
-    console.log('message received!')
-    const data = msg.getData()
-    if (typeof data === 'string') {
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`)
-    }
-
-    msg.ack()
-  })
+  new TicketCreatedListener(stan).listen()
 })
 
 process.on('SIGINT', () => stan.close())
 process.on('SIGTERM', () => stan.close())
+
+abstract class Listener {
+  abstract subject: string
+  abstract queueGroupName: string
+  protected actWait = 5 * 1000
+
+  constructor(private readonly client: Stan) {}
+
+  abstract onMessage(data: any, msg: Message): void
+
+  subscriptionOptions() {
+    // manual acknowledgement of the events
+    // the default is false as soon the event received we will not received the event again.
+    return this.client.subscriptionOptions()
+      .setDeliverAllAvailable()
+      .setManualAckMode(true)
+      .setAckWait(this.actWait)
+      .setDurableName(this.queueGroupName)
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    )
+
+    subscription.on('message', (msg: Message) => {
+      console.log(
+        `Message Received: ${this.subject} / ${this.queueGroupName}`
+      )
+
+      const parsedData = this.parseMessage(msg)
+
+      this.onMessage(parsedData, msg)
+    })
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData()
+    return typeof data === 'string'? JSON.parse(data): JSON.parse(data.toString('utf8'))
+  }
+}
+
+class TicketCreatedListener extends Listener {
+  queueGroupName: string = 'payments-service';
+  subject: string = 'ticket:created';
+
+  onMessage(data: any, msg: Message): void {
+    console.log('Event data!', data)
+    msg.ack()
+  }
+}
