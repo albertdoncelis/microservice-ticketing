453fd96239f14cb2b0990a411e0ea0e4fd3bfb75
Albert Don Celis <celisalbertdon@gmail.com>
2020-11-20 19:35:34 +0800

    update

--- tickets/src/nats-wrapper.ts
+++ tickets/src/nats-wrapper.ts
@@ -10,6 +10,7 @@ class NatsWrapper {
 
     return this._client
   }
+
   connect(clusterId: string, clientId: string, url: string) {
     this._client = nats.connect(clusterId, clientId, { url })
 
