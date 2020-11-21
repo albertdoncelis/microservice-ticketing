1e89fe8194c6b134ce54830a23114b713bf79dc3
Albert Don Celis <celisalbertdon@gmail.com>
2020-11-21 11:34:03 +0800

    add order depl, service, update skaffold and create order logic

---
+++ orders/src/models/ticket.ts
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose'
+
+interface TicketAttrs {
+  title: string,
+  price: number,
+}
+
+export interface TicketDoc extends mongoose.Document, TicketAttrs {}
+
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(attr: TicketAttrs): TicketDoc
+}
+
+const ticketSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0
+  }
+}, {
+  toJSON: {
+    transform(doc, ret) {
+      ret.id = ret._id
+      delete ret._id
+    }
+  }
+})
+
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+  return new Ticket(attrs)
+}
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema)
+
+export { Ticket }
