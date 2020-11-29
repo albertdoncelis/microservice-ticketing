6bb92e542af32f0497b38dd1f2af52c84cba6310
Albert Don Celis <celisalbertdon@gmail.com>
2020-11-29 17:40:11 +0800

    update payment api without stripe

---
+++ payments/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,26 @@
+import { Listener, OrderCancelledEvent, OrderStatus, Subjects } from "@acelistickets/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+import { Order } from "../../models/order";
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+  async onMessage(data: OrderCancelledEvent["data"], msg: Message) {
+    const order = await Order.findOne({
+      _id: data.id,
+      version: data.version - 1
+    })
+
+    if (!order) {
+      throw new Error('Order not found')
+    }
+
+    order.set({ status: OrderStatus.Cancelled })
+    await order.save()
+
+    msg.ack()
+  }
+
+  queueGroupName: string = queueGroupName;
+  readonly subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
+
+}
