dc89499e88c4c5c65fcf9835cb322675f8e2504c
Albert Don Celis <celisalbertdon@gmail.com>
2020-12-13 07:54:20 +0800

    last minute change

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -17,7 +17,8 @@ it('response with null if not authenticated', async () => {
   const response = await request(app)
     .get('/api/users/currentuser')
     .send()
-    .expect(200)
+    .expect(400)
+    // .expect(200)
 
   expect(response.body.currentUser).toEqual(null)
 })
